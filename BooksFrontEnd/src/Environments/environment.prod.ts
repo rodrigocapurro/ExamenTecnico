ba9c3bb1254383b1c30764003d676f9743d9b56b
RodrigoCapurro <rodrigomartincapurro@hotmail.com>
2023-10-18 13:31:48 -0300

    Delete BooksFrontEnd/src directory

--- BooksFrontEnd/src/Environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiURL: 'http://localhost/BooksApi/api/books',
-    apiNetCoreURL: 'https://localhost:7285/api/BooksNetCore'
-  };
--- BooksFrontEnd/src/Environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiURL: 'http://localhost/BooksApi/api/books',
-    apiNetCoreURL: 'https://localhost:7285/api/BooksNetCore'
-  };
