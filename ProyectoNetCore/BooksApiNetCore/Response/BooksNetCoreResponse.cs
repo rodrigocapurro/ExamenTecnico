c2f36b4f078a2cdaf31447d5e959ede48d276818
RodrigoCapurro <rodrigomartincapurro@hotmail.com>
2023-10-18 13:41:32 -0300

    Delete ProyectoNetCore directory

--- ProyectoNetCore/BooksApiNetCore/Response/BooksNetCoreResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using BooksNetCoreDto.ModelNetCore;
-
-namespace BooksApiNetCore.Response
-{
-    public class BooksNetCoreResponse: BaseNetCoreResponse
-    {
-        public List<BookModelNetCore> booksList { get; set; }
-    }
-}
